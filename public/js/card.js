67c69ad0cb87c1bc102d926b708c6450c94ef62d
Anurag Banerjee <anuragb2010@gmail.com>
2016-07-16 11:59:50 -0700

    Add label on progress bar

--- public/js/card.js
+++ public/js/card.js
@@ -1,27 +1,27 @@
-function getSelectedText() {
-    var text = "";
-
-    if (typeof window.getSelection != "undefined") {
-        text = window.getSelection().toString();
-    } else if (typeof document.selection != "undefined" && document.selection.type == "Text") {
-        text = document.selection.createRange().text;
-    }
-
-    return text;
-}
-
-function doSomethingWithSelectedText() {
-    var selectedText = getSelectedText();
-
-    if (selectedText) {
-        alert("Got selected text:\n" + selectedText);
-    }
-}
-
-document.onmouseup = doSomethingWithSelectedText;
-document.onkeyup = doSomethingWithSelectedText;
-
-// $('div').mouseup(function() {
-//     var text=getSelectedText();
-//     if (text!='') alert(text);
-// });
+// function getSelectedText() {
+//     var text = "";
+//
+//     if (typeof window.getSelection != "undefined") {
+//         text = window.getSelection().toString();
+//     } else if (typeof document.selection != "undefined" && document.selection.type == "Text") {
+//         text = document.selection.createRange().text;
+//     }
+//
+//     return text;
+// }
+//
+// function doSomethingWithSelectedText() {
+//     var selectedText = getSelectedText();
+//
+//     if (selectedText) {
+//         alert("Got selected text:\n" + selectedText);
+//     }
+// }
+//
+// document.onmouseup = doSomethingWithSelectedText;
+// document.onkeyup = doSomethingWithSelectedText;
+//
+// // $('div').mouseup(function() {
+// //     var text=getSelectedText();
+// //     if (text!='') alert(text);
+// // });
